3ec6c44c9d1fa09fca566bf3c3fe75d7f29f1190
Jeta Derveni <jetaderveni@outlook.com>
2025-09-07 05:10:32 +0200

    Testing (parziale)

--- StudyBuddies/social/urls.py
+++ StudyBuddies/social/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('friend-list/remove/<int:user_id>/', views.remove_friend, name="remove_friend"),
     path('friend-request/<int:user_id>/', views.send_friend_request, name='send_friend_request'),
     path('friend-requests/', views.view_friend_requests, name='friend_requests'),
-    path('friend-request/<int:friend_req_id>/accept>/', views.accept_friend_request, name='accept_friend_request'),
+    path('friend-request/<int:friend_req_id>/accept/', views.accept_friend_request, name='accept_friend_request'),
     path('friend-request/<int:friend_req_id>/reject/', views.reject_friend_request, name='reject_friend_request'),
     
     
--- StudyBuddies/study/tests.py
+++ StudyBuddies/study/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import StudySession, Subject
+from django.urls import reverse
+from datetime import datetime
 
-# Create your tests here.
+User = get_user_model()
+class StudySessionTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="usernametesting", password="beepboop")
+        self.subject =Subject.objects.create(name='matematica')
+        self.client.login(username='usernametesting', password='beepboop')
+
+    def test_study_session_create(self):
+        session=StudySession.objects.create(user = self.user, subject =self.subject, duration = 2.5, date=datetime.now().date(), notes='session created by unit test')
+        self.assertEqual(session.duration, 2.5)
+        self.assertEqual(session.user.username, 'usernametesting')
+        self.assertEqual(session.subject.name, 'matematica')
+        self.assertIsInstance(session.subject, Subject)
+
+        session2 = StudySession.objects.create(user = self.user, subject =self.subject, duration =25.0, date=datetime.now().date(), notes='session2created by unit test1')
+
+
+    def test_study_session_list(self):
+        response = self.client.get(reverse('study:session_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'study/session_list.html')
